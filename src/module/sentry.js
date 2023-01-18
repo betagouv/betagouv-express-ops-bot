6d55466850aae8991c0010996d9c5de8e854d3d4
Lucas Charrier <lucharrier@gmail.com>
2023-01-18 10:04:48 +0100

    Fix : fix getting function params

--- src/module/sentry.js
+++ src/module/sentry.js
@@ -2,7 +2,7 @@
 const { setTimeout } = require("timers/promises");
 
 async function sentry_member(req, res) {
-    const cmdParams = cmd.replace(arguments.callee.name, '').split(' ').map(param => param.trim())
+    const cmdParams = req.body.text.split(' ').map(param => param.trim())
     await setTimeout(5000);
     res.json({
         text: 'sentry response',
