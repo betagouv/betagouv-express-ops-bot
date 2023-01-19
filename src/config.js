fae7a5cb05d84df9d08b4d5223bbe6803af25512
Lucas Charrier <lucharrier@gmail.com>
2023-01-19 16:02:00 +0100

    Feat : add BOT_ENDPOINT to config

--- src/config.js
+++ src/config.js
@@ -7,5 +7,6 @@ module.exports = {
 		'Authorization': 'Bearer ' + process.env.BETAOPSBOT_API_KEY
 	},
 	endpoint: process.env.BETAOPSBOT_API_ENDPOINT,
-	airtable_endpoint: process.env.BETAOPSBOT_ALWAYSDATA_WEBHOOK
+	airtable_endpoint: process.env.BETAOPSBOT_ALWAYSDATA_WEBHOOK,
+	BOT_ENDPOINT: process.env.BOT_ENDPOINT
 }
