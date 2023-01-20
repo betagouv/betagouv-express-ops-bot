32fbe1f14c886d5606246e7389a33b7d360cd68d
Lucas Charrier <lucharrier@gmail.com>
2023-01-20 12:11:23 +0100

    Core : change to prefix private value with underscore

--- src/services/inmemory.js
+++ src/services/inmemory.js
@@ -2,22 +2,22 @@
 module.exports = {
     IN_MEMORY_DB: {
         _postInfos: {},
-        callbackIds: [],
+        _callbackIds: [],
         add_post_info: function(postId, data) {
-            return this.postInfos[postId] = data
+            return this._postInfos[postId] = data
         },
         get_post_info: function(postId) {
-            return this.postInfos[postId]
+            return this._postInfos[postId]
         },
         add_callback_id: function(callback_id) {
-            this.callbackIds.push(callback_id)
+            this._callbackIds.push(callback_id)
         },
         verify_callback_id: function(callback_id) {
             let verified = false
-            const index = this.callbackIds.findIndex((id) => { return id === callback_id })
+            const index = this._callbackIds.findIndex((id) => { return id === callback_id })
             if (index !== -1) {
                 verified = true
-                this.callbackIds.splice(index, 1)
+                this._callbackIds.splice(index, 1)
             }
             return verified
         }
