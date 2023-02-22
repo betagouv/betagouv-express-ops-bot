861c5e8e9750b3078385b244b70702684d6d4ff7
Charrier <lucharrier@gmail.com>
2023-02-22 17:35:11 +0100

    Update scalingo.js

--- src/plugins/scalingo.js
+++ src/plugins/scalingo.js
@@ -1,15 +1,15 @@
 const helper = require('../services/helper')
 const config = require('../config')
 
-async function scalingoapp(ctx, app_name, app_region, app_collaborator) {
+async function scalingoapp(ctx, name, region, collaborator) {
     const params = {
-        app_name,
-        app_region,
-        app_collaborator
+        name,
+        region,
+        collaborator
     }
     const errors = {}
-    helper.isValidEmail(app_collaborator, (error) => { errors['app_collaborator'] = error })
-    helper.isValidRegion(app_region, (error) => { errors['app_region'] = error })
+    helper.isValidEmail(collaborator, (error) => { errors['app_collaborator'] = error })
+    helper.isValidRegion(region, (error) => { errors['app_region'] = error })
     if (errors.length) {
         return `Erreur de params : ${JSON.stringify(errors)}`
     }
@@ -18,7 +18,7 @@ async function scalingoapp(ctx, app_name, app_region, app_collaborator) {
     return helper.call(ctx, api_endpoint,
         params,
         {
-            finished: `Scalingo: creation de l'app ${app_name}, ${app_region}, with ${app_collaborator} :white_check_mark:`,
+            finished: `Scalingo: creation de l'app ${name}, ${region}, with ${collaborator} :white_check_mark:`,
             error: `Scalingo: la tâche ${ctx.post_id} n'est pas fini après 10s, il doit y avoir une erreur`
         }
     )
